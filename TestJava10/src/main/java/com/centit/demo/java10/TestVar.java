e5e92e53361b8e2d6da8a0e4bbadeeab82e2b554
杨淮生 <codefan@centit.com>
2018-05-18 14:19:44 +0800

    添加 java10 和 kotlin 的测试环境

---
+++ TestJava10/src/main/java/com/centit/demo/java10/TestVar.java
@@ -0,0 +1,11 @@
+package com.centit.demo.java10;
+
+import java.time.Instant;
+
+public class TestVar {
+    public static void main(String [] args){
+        for(var i = 5; i<15; i++){
+            System.out.println(i);
+        }
+    }
+}
---
+++ TestKotlin/src/com/centit/demo/kotlin/TestKotlin.kt
@@ -0,0 +1,16 @@
+package com.centit.demo.kotlin
+
+//Array<String>
+fun main(vararg args: String) {
+    var a = 1
+// 模板中的简单名称：
+    val s1 = "a is $a"
+    println(s1)
+    a = 2
+// 模板中的任意表达式：
+    val s2 = "${s1.replace("is", "was")}, but now is $a"
+    println(s2)
+
+    val sumLambda: (Int, Int) -> Int = {x,y ->  x + y }
+    println(sumLambda(1,2))  // 输出 3
+}
