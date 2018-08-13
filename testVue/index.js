c6b2c62aec60bed0fb498abdb73411ef717e9a06
杨淮生 <codefan@centit.com>
2018-08-13 10:54:57 +0800

    测试Vue

--- testVue/index.js
+++ testVue/index.js
@@ -1,4 +1,13 @@
 
+Vue.component('button-counter', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+})
+
 Vue.component('todo-item', {
   props: ['todo'],
   template: '<li>{{ todo.text }}</li>'
