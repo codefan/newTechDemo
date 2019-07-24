f700a8d6cd3514fa1a68e26a1ad1b79cd08d3a3e
codefan <codefan@sina.com>
2019-07-24 15:17:24 +0800

    添加 Netty实现的http 示例

--- TestNetty/src/main/java/com/centit/demo/netty/boot/NettyHttpServerHandler.java
+++ TestNetty/src/main/java/com/centit/demo/netty/boot/NettyHttpServerHandler.java
@@ -1,13 +1,10 @@
 package com.centit.demo.netty.boot;
 
-import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
-import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.codec.http.*;
-import io.netty.util.CharsetUtil;
 
 /**
  * 代码清单 2-1 EchoServerHandler
@@ -17,19 +14,35 @@ import io.netty.util.CharsetUtil;
 //标示一个ChannelHandler可以被多个 Channel 安全地共享
 @Sharable
 public class NettyHttpServerHandler extends ChannelInboundHandlerAdapter {
+    public static void writeResponeToHttpClient(ChannelHandlerContext ctx, String content) throws Exception{
+        FullHttpResponse response = new DefaultFullHttpResponse(
+            HttpVersion.HTTP_1_1,
+            HttpResponseStatus.OK,
+            Unpooled.wrappedBuffer(content.getBytes("utf-8")));
+        response.headers().set(HttpHeaders.Names.CONTENT_TYPE, "text/plain;charset=UTF-8");
+        response.headers().set(HttpHeaders.Names.CONTENT_LENGTH, response.content().readableBytes());
+        response.headers().set(HttpHeaders.Names.CONNECTION, HttpHeaders.Values.KEEP_ALIVE);
+        ctx.write(response);
+        ctx.flush();
+    }
+
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object request) throws Exception {
-        if(request instanceof HttpRequest) {
-            FullHttpResponse response = new DefaultFullHttpResponse(
-                HttpVersion.HTTP_1_1,
-                HttpResponseStatus.OK,
-                Unpooled.wrappedBuffer(("您访问了：" + ((HttpRequest) request).uri()).getBytes("utf-8")));
-            response.headers().set(HttpHeaders.Names.CONTENT_TYPE, "text/plain;charset=UTF-8");
-            response.headers().set(HttpHeaders.Names.CONTENT_LENGTH, response.content().readableBytes());
-            response.headers().set(HttpHeaders.Names.CONNECTION, HttpHeaders.Values.KEEP_ALIVE);
-            ctx.write(response);
-            ctx.flush();
+        if (request instanceof HttpRequest) {
+            String responseContent = "您访问了" + ((HttpRequest) request).method().toString() +":" + ((HttpRequest) request).uri();
+            NettyHttpServerHandler.writeResponeToHttpClient(ctx, responseContent);
         }
+
+        /*else if (request instanceof HttpContent) {
+            HttpContent httpContent = (HttpContent) request;
+            ByteBuf byteData = httpContent.content();
+            if (byteData instanceof EmptyByteBuf) {
+                responseContent = "Content：无数据！";
+            } else {
+                String content = byteData.toString();
+                responseContent = "Content:" + content;
+            }
+        }*/
     }
 
     @Override
