b9daa20f3f1938930d979330cdb2d053072f55c7
codefan <codefan@sina.com>
2021-04-30 13:45:54 +0800

    添加 webflux oauth2.0

---
+++ TestWebFlux/src/main/java/com/centit/demo/webflux/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.centit.demo.webflux.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+/**
+ * @author Rob Winch
+ */
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityWebFilterChain configure(ServerHttpSecurity http) {
+        http
+            .authorizeExchange(exchanges ->
+                exchanges
+                    .pathMatchers("/","/login** ","/hello/**").permitAll()
+                    .anyExchange().authenticated()
+            )
+            .oauth2Login(withDefaults())
+            .formLogin(withDefaults())
+            .oauth2Client(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    MapReactiveUserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withDefaultPasswordEncoder()
+            .username("user")
+            .password("password")
+            .roles("USER")
+            .build();
+        return new MapReactiveUserDetailsService(userDetails);
+    }
+}
